3835248fbb78f9cf2294dbaa48c3883f3e9fd5e5
RomanSheremet <roma.sheremet.91@gmail.com>
2022-01-02 16:19:04 +0300

    feature/add description

--- src/Users/users.controller.ts
+++ src/Users/users.controller.ts
@@ -1,17 +1,23 @@
 import {Body, Controller, Get, Post} from '@nestjs/common';
 import {UsersService} from "./users.service";
 import {CreateUserDto} from "./Dto/CreateUser.dto";
+import {ApiOperation, ApiResponse} from "@nestjs/swagger";
+import {User} from "./users.model";
 
 @Controller('users')
 export class UsersController {
 
     constructor(private usersService: UsersService) {}
 
+    @ApiOperation({summary: "Добавление пользователя"})
+    @ApiResponse({status: 200, type: User})
     @Post()
     createNewUser(@Body() userDto: CreateUserDto) {
         return this.usersService.createUser(userDto);
     }
 
+    @ApiOperation({summary: "Получение всех пользователей"})
+    @ApiResponse({status: 200, type: [User]})
     @Get()
     getAllUsers() {
         return this.usersService.getAllUsers();
--- src/Users/users.model.ts
+++ src/Users/users.model.ts
@@ -1,4 +1,5 @@
 import {Column, DataType, Model, Table} from "sequelize-typescript";
+import {ApiProperty} from "@nestjs/swagger";
 
 interface UserCreationAttributes {
     email: string;
@@ -7,18 +8,23 @@ interface UserCreationAttributes {
 
 @Table({tableName: 'users'})
 export class User extends Model<User, UserCreationAttributes>{
+    @ApiProperty({example: "01", description: "айдишник пользователя"})
     @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
     id: number;
 
+    @ApiProperty({example: "roma.sheremet.91@mail.ru", description: "имейл пользователя"})
     @Column({type: DataType.STRING, unique: true, allowNull: false})
     email: string;
 
+    @ApiProperty({example: "dgfFDGgfdg74645$#dfgds", description: "пароль пользователя"})
     @Column({type: DataType.STRING, allowNull: false})
     password: string;
 
+    @ApiProperty({example: "false", description: "забанен ли пользователь"})
     @Column({type: DataType.BOOLEAN, allowNull: false, defaultValue: false})
     isBanned: boolean;
 
+    @ApiProperty({example: "Оскорблял других людей", description: "причина бана"})
     @Column({type: DataType.STRING})
     banReason: string;
 }
