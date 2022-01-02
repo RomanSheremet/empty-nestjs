59ae6b717d94717d404db7b066b07645f4f3008f
RomanSheremet <roma.sheremet.91@gmail.com>
2022-01-02 16:22:26 +0300

    feature/add description

--- src/Users/users.controller.ts
+++ src/Users/users.controller.ts
@@ -1,9 +1,10 @@
 import {Body, Controller, Get, Post} from '@nestjs/common';
 import {UsersService} from "./users.service";
 import {CreateUserDto} from "./Dto/CreateUser.dto";
-import {ApiOperation, ApiResponse} from "@nestjs/swagger";
+import {ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
 import {User} from "./users.model";
 
+@ApiTags('Пользователи')
 @Controller('users')
 export class UsersController {
 
