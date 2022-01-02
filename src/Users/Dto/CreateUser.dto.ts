5b949752f53567b51cf4e7b955aedd44ece77515
RomanSheremet <roma.sheremet.91@gmail.com>
2022-01-02 16:20:53 +0300

    feature/add description

--- src/Users/Dto/CreateUser.dto.ts
+++ src/Users/Dto/CreateUser.dto.ts
@@ -1,4 +1,8 @@
+import {ApiProperty} from "@nestjs/swagger";
+
 export class CreateUserDto {
+    @ApiProperty({example: "roma.sheremet.91@mail.ru", description: "имейл пользователя"})
     email: string;
+    @ApiProperty({example: "sgsdfFDGSGgfgGFG543", description: "пароль пользователя"})
     password: string;
 }
